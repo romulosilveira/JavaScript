614638222918fc7aba580368c1e8bd6247d9b6e4
Rômulo Silveira <150481220+romulosilveira@users.noreply.github.com>
2024-05-31 18:34:58 -0300

    Add files via upload

---
+++ AULA4_JS/atividade_prat_3.js
@@ -0,0 +1,15 @@
+let n = Number(prompt("Digite o número desejado: "))
+let x = 0
+
+if(n<0){
+    x = (n)*(-1)}
+else if(x = n)
+
+while(x>=0){
+    console.log(`${x}`)
+    x--
+}
+
+//for(let i = n; i >= 0; i--){
+//    console.log(i)
+//}
