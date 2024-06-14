a51ffa5fddc0b24393eb0df069931324c97091f3
Rômulo Silveira <150481220+romulosilveira@users.noreply.github.com>
2024-06-14 18:48:16 -0300

    Add files via upload

---
+++ AULA6_JS/funcao_aula6.js
@@ -0,0 +1,15 @@
+// function demaior(idade){
+//     if(idade >= 18){
+//         return "Maior de idade"}
+//     else{
+//         return "Menor de idade"}
+// }
+
+// const x_idade = Number(prompt("Digite sua idade: "))
+
+// document.write(demaior(x_idade))
+
+function vogais(){
+    const nome = prompt("Digite seu nome: ")
+    return nome.length
+}
